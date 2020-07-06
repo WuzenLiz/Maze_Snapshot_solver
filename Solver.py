ace408a8f49ec2037a28f825a8fca5ec189de6a6
macgia99 <macgia1999@gmail.com>
2020-07-07 03:57:12 +0700

    Update new function

--- Solver.py
+++ Solver.py
@@ -1,16 +1,16 @@
 from maze import Maze
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from AStar import solve
+import numpy as np
 import time
 Image.MAX_IMAGE_PIXELS = None
 
 
 def Mazer_solver(input_file, output_file):
     # Load Image
-    print("Loading Image")
     im = Image.open(input_file)
     # Create the maze (and time it) - for many mazes this is more time consuming than solving the maze
-    print("Load to Maze")
+    print("\nLoad node from image to Maze")
     t0 = time.time()
     maze = Maze(im)
     t1 = time.time()
@@ -50,7 +50,7 @@ def Mazer_solver(input_file, output_file):
 
         # Blue - red
         r = int((i / length) * 255)
-        px = (r, 0, 255 - r)
+        px = (r, r-r//2, 255 - r)
 
         if a[0] == b[0]:
             # Ys equal - horizontal line
@@ -63,11 +63,35 @@ def Mazer_solver(input_file, output_file):
     im.save(output_file, quality=100, subsampling=0)
 
 
-def resize_for_better_look(input_file):
+def resize_for_better_look(input_file, text):
     im = Image.open(input_file)
-    width, height = im.size
-    newsize = (width*4, height*4)
-    im = im.convert('RGBA')
-    im = im.resize(newsize, Image.ANTIALIAS)
-    im.save(input_file, quality=100, subsampling=0)
-    im.show()
+    w, h = im.size
+    newsize = (w*5, h*5)
+    im = im.resize(newsize)
+    new_im_size = (w*5+10, h*5+35)
+    new_im = Image.new('RGB', new_im_size)
+    new_im.paste(im, (5, 30))
+    draw = ImageDraw.Draw(new_im)
+    font = ImageFont.truetype('arial.ttf', 20)
+    draw.text((new_im_size[1]/5, 5), text, (255, 255, 255), font=font)
+    new_im.save(input_file, quality=100, subsampling=0)
+
+
+def create_compare_image(input_file, output_file, compare_file):
+    resize_for_better_look(input_file, "Original Maze")
+    resize_for_better_look(output_file, "Solved Maze")
+    imgs = [Image.open(i) for i in [input_file, output_file]]
+    # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
+    widths, heights = zip(*(i.size for i in imgs))
+    total_width = sum(widths)
+    max_height = max(heights)
+    new_im = Image.new('RGB', (total_width, max_height))
+    # Place first image
+    new_im.paste(imgs[0], (0, 0))
+
+    # Iteratively append images in list horizontally
+    hoffset = 0
+    for i in range(1, len(imgs), 1):
+        hoffset = imgs[i-1].size[0]+hoffset
+        new_im.paste(imgs[i], (hoffset, 0))
+    new_im.save(compare_file, quality=100, subsampling=0)
--- main.py
+++ main.py
@@ -1,48 +1,87 @@
-from Solver import Mazer_solver
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from io import BytesIO
+import sys
+import getopt
 import time
 import base64
-
-"""Setting webdriver"""
-driver = webdriver.Chrome('./Webdriver/chromedriver.exe')
-t0 = time.time()
-driver.get("https://keesiemeijer.github.io/maze-generator/")
+from Solver import Mazer_solver, create_compare_image
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from PIL import Image
+from io import BytesIO
 input_path = './maze/maze_for_solve.png'
 output_path = './maze/solved_maze.png'
-
-"""Prepare and generate maze"""
-# maze width input
-maze_w = driver.find_element_by_xpath(
-    "//input[@name='width' and @value='20']")
-driver.execute_script("arguments[0].setAttribute('value', '40')", maze_w)
-# maze height input
-maze_h = driver.find_element_by_xpath(
-    "//input[@name='height' and @value='20']")
-driver.execute_script("arguments[0].setAttribute('value', '40')", maze_h)
-# fix wall size
-maze_wall_size = driver.find_element_by_id("wall-size")
-driver.execute_script(
-    "arguments[0].setAttribute('value', '1')", maze_wall_size)
-# select for start/end at top/bottop
-Select(driver.find_element_by_xpath(
-    "//select[@id='entry' and @name='entry']")).select_by_value('vertical')
-# click on
-driver.find_elements_by_id("create-maze")[0].click()
-
-"""Get maze image"""
-maze_img_path = driver.find_element_by_xpath("//canvas[@id='maze']")
-js = "return arguments[0].toDataURL('image/png').substring(21)"
-maze_img_to_base64 = driver.execute_script(js, maze_img_path)
-maze_img = base64.b64decode(maze_img_to_base64)
-with open(input_path, 'wb') as f:
-    f.write(maze_img)
-driver.close()
+compare_path = './maze/compare_image.png'
 
 
-"""solve the maze"""
-Mazer_solver(input_path, output_path)
-t1 = time.time()
-total = t1-t0
-print("Tổng thời gian thực hiện:", total, "\n")
+def Get_image(width, height):
+    """Setting webdriver"""
+    WDoptions = Options()
+    WDoptions.add_argument('--headless')
+    WDoptions.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(
+        './Webdriver/chromedriver.exe', options=WDoptions)
+    driver.get("https://keesiemeijer.github.io/maze-generator/")
+    """Prepare and generate maze"""
+    # maze width input
+    print("Generate Maze", width, "*", height)
+    maze_w = driver.find_element_by_xpath(
+        "//input[@name='width' and @value='20']")
+    driver.execute_script(
+        "arguments[0].setAttribute('value', arguments[1])", maze_w, width)
+    # maze height input
+    maze_h = driver.find_element_by_xpath(
+        "//input[@name='height' and @value='20']")
+    driver.execute_script(
+        "arguments[0].setAttribute('value', arguments[1])", maze_h, height)
+    # fix wall size
+    maze_wall_size = driver.find_element_by_id("wall-size")
+    driver.execute_script(
+        "arguments[0].setAttribute('value', '1')", maze_wall_size)
+    # select for start/end at top/bottop
+    Select(driver.find_element_by_xpath(
+        "//select[@id='entry' and @name='entry']")).select_by_value('vertical')
+    # click on
+    driver.find_elements_by_id("create-maze")[0].click()
+    """Get maze image"""
+    maze_img_path = driver.find_element_by_xpath(
+        "//canvas[@id='maze']")
+    js = "return arguments[0].toDataURL('image/png').substring(21)"
+    maze_img_to_base64 = driver.execute_script(js, maze_img_path)
+    maze_img = base64.b64decode(maze_img_to_base64)
+    im = Image.open(BytesIO(maze_img))
+    im.save(input_path, quality=100, subsampling=0)
+    driver.close()
+
+
+def main(argv):
+    h = w = 40
+    try:
+        opts, args = getopt.getopt(
+            argv, "w:h:?", ["Width=", "Height=", "width=", "height="])
+    except getopt.GetoptError:
+        print('get_image.py -w <Maze Width> -h <Maze Height>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-?':
+            print('get_image.py -w <Maze Width> -h <Maze Height>')
+            sys.exit()
+        elif opt in ("-w", "--Width", "--width"):
+            w = arg
+        elif opt in ("-h", "--Height", "--height"):
+            h = arg
+        elif opt == '':
+            w = h = 40
+    t0 = time.time()
+    Get_image(w, h)
+    """solve the maze"""
+    Mazer_solver(input_path, output_path)
+    create_compare_image(input_path, output_path, compare_path)
+    Image.open(compare_path).show()
+    """Time count"""
+    t1 = time.time()
+    total = t1-t0
+    print("Done in:", total, "\n")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
